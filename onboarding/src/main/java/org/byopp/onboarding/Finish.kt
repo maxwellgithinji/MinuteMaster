d809db8b2b1ed5cca40cd4878b0080d507e4bd1a
maxwellgithinji <maxwellgithinji@gmail.com>
2024-07-09 19:42:45 -0400

    finish onboarding - setup onboarding structure of the app

--- onboarding/src/main/java/org/byopp/onboarding/Finish.kt
+++ onboarding/src/main/java/org/byopp/onboarding/Finish.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import org.byopp.minutemaster.ui.theme.MinuteMasterTheme
 
@@ -33,10 +36,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column {
+    Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
         Text(
-            text = "Hello $name!",
-            modifier = modifier
+            text = "Read the question",
+            modifier = modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
         )
     }
 }
